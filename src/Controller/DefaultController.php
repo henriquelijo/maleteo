968bc87f08da7c0380e4fc1d67f2829ecb6d8ef0
Henrique Lijó <henriquelijo@gmail.com>
2021-02-03 21:04:51 +0100

    mércores-tarde

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Controller\Form\FormType;
+use App\Form\MessageFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,16 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/maletea", name="homepape")
+     * @Route("/maletea", name="homepage")
      */
 
     public function homepage (Request $r)
     {
         $form = $this ->createForm(FormType::class);
+        $messaForm = $this->createForm(MessageFormType::class)
         //$form = $this->createForm(‘App\Form\FormType’); Hace lo mismo que la línea anterior.
 
         return $this->render('base.html.twig', [
-            'formulario'=>$form->createView()
+            'formulario'=>$form->createView(),
+            'messageForm'=>$messaForm->createView()
         ]);
 
     }
--- src/Controller/messageFormController.php
+++ src/Controller/messageFormController.php
@@ -4,9 +4,11 @@
 namespace App\Controller;
 
 
+use App\Entity\FormMaleteo;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class messageFormController extends AbstractController
 {
@@ -14,6 +16,30 @@ class messageFormController extends AbstractController
      * @Route("/message", name="message")
      */
     public function sendMessage (Request $r, EntityManagerInterface $doctrine)
+    {
         $form = $this->createForm(MessageFormType::class);
+        $form->handleRequest($r);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $datos = $form-getData();
+
+            $message = new FormMaleteo();
+            $message->setUser($datos["user"]);
+            $message->setMessage($datos['message']);
+
+            $doctrine->persist($message);
+            $doctrine->flush();
+
+            $this->addFlash('sucess', 'su comentario ha sido añadido');
+            return $this->redirect('homepage');
+        }else{
+            return $this->render('base.html.twig', [
+                'messageForm' => $form->createView()
+            ]);
+        }
+
+    }
+
+
 
 }
